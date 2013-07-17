48dcf1846d4b6e8b0ea1aa12298d8fde7101a41c
Pavel <php_prog@mail.ru>
2013-07-17 21:22:33 +0300

    The first step in the BEM. (Anketa, title, about)

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,30 +1,34 @@
 ({
     block: 'b-page',
-    title: 'Title of the page',
-    favicon: '/favicon.ico',
+    title: 'Анкета второй Школы разработки интерфейсов в Москве',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index', ie: true },
-        { elem: 'meta', attrs: { name: 'description', content: '' }},
-        { elem: 'meta', attrs: { name: 'keywords', content: '' }}
+        { elem: 'meta', attrs: { name: 'description', content: 'Форму-анкета с заданиями для кандидатов' }}
     ],
-    content:[
+    content: [
         {
-            block: 'header',
+            block: 'b-anketa',
             content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
+                {
+                    elem: 'title',
+                    content: 'Анкета второй Школы разработки интерфейсов в Москве'
+                },
+                {
+                    block: 'b-about',
+                    content: [
+                        { elem: 'message', tag: 'p', content: 'Яндекс открывает набор во вторую Школу разработки интерфейсов в Москве.' },
+                        { elem: 'message', tag: 'p', content: 'Для поступления необходимо заполнить анкету с тестовыми заданиями. Приём заявок закрывается 19 августа в 23:59 по московскому времени. По результатам теста мы пригласим в Школу 30-40 человек.' },
+                        { elem: 'message',
+                            tag: 'p',
+                            content: {
+                                tag: 'a', attrs: { href: 'http://events.yandex.ru/shri/' },
+                                url: 'http://bem.info',
+                                content: 'Подробности о Школе читайте на этой странице.'
+                            }
+                        }
+                    ]
+                }
             ]
         },
         { block: 'i-jquery', mods: { version: '1.8.3' } },
