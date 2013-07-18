ef6892b76e04de0d978b5ded9ffa928e62c31d89
Pavel <php_prog@mail.ru>
2013-07-18 13:31:36 +0300

    Set link in string, padding-bottom for title, line-height: 1.4em;

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -4,28 +4,32 @@
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index', ie: true },
-        { elem: 'meta', attrs: { name: 'description', content: 'Форму-анкета с заданиями для кандидатов' }}
+        { elem: 'meta', attrs: { name: 'description', content: 'Форма-анкета с заданиями для кандидатов' }}
     ],
     content: [
         {
             block: 'b-anketa',
             content: [
                 {
-                    elem: 'title',
+                       elem: 'title',
+                        tag: 'h1',
                     content: 'Анкета второй Школы разработки интерфейсов в Москве'
                 },
                 {
                     block: 'b-about',
                     content: [
-                        { elem: 'message', tag: 'p', content: 'Яндекс открывает набор во вторую Школу разработки интерфейсов в Москве.' },
-                        { elem: 'message', tag: 'p', content: 'Для поступления необходимо заполнить анкету с тестовыми заданиями. Приём заявок закрывается 19 августа в 23:59 по московскому времени. По результатам теста мы пригласим в Школу 30-40 человек.' },
-                        { elem: 'message',
-                            tag: 'p',
-                            content: {
-                                tag: 'a', attrs: { href: 'http://events.yandex.ru/shri/' },
-                                url: 'http://bem.info',
-                                content: 'Подробности о Школе читайте на этой странице.'
-                            }
+                        { tag: 'p', content: 'Яндекс открывает набор во вторую Школу разработки интерфейсов в Москве.' },
+                        { tag: 'p', content: 'Для поступления необходимо заполнить анкету с тестовыми заданиями. Приём заявок закрывается 19 августа в 23:59 по московскому времени. По результатам теста мы пригласим в Школу 30-40 человек.' },
+                        { tag: 'p',
+                            content: [
+                                'Подробности о Школе читайте ',
+                                {
+                                        tag: 'a', attrs: { href: 'http://events.yandex.ru/shri/' },
+                                        url: 'http://bem.info',
+                                    content: 'на этой странице'
+                                },
+                                '.'
+                            ]
                         }
                     ]
                 }
