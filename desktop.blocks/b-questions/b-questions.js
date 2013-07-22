1549d93fd66539ff1bb5b240cf14105a0aebd859
Pavel <php_prog@mail.ru>
2013-07-22 09:07:55 +0300

    run-up bemhtml questions

---
+++ desktop.blocks/b-questions/b-questions.js
@@ -0,0 +1,24 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function(undefined) {
+
+BEM.DOM.decl('b-questions', {
+
+    onSetMod : {
+
+        'js' : function() {
+            /* ... */
+        }
+
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
