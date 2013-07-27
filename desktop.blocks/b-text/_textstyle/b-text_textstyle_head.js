1ed4dea5575e56e0ea0b868104ac1fb67d82be4f
Pavel <php_prog@mail.ru>
2013-07-27 15:30:15 +0300

    Start from scratch.

---
+++ desktop.blocks/b-text/_textstyle/b-text_textstyle_head.js
@@ -0,0 +1,24 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function(undefined) {
+
+BEM.DOM.decl({ block: 'b-text', modName: 'textstyle', modVal: 'head'}, {
+
+    onSetMod : {
+
+        'js' : function() {
+            /* ... */
+        }
+
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
